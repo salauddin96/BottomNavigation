097aecf9508e7b0bee23524b87de1d60c79fe116
salauddin96 <salauddin33-5088@diu.edu.bd>
2023-09-25 13:24:42 +0600

    update lines

--- app/src/main/java/org/techtales/bottomnavapp/HomeFragment.kt
+++ app/src/main/java/org/techtales/bottomnavapp/HomeFragment.kt
@@ -5,14 +5,49 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 
 
 class HomeFragment : Fragment() {
+    private lateinit var tvsec: TextView
+    lateinit var tvMain: TextView
+    lateinit var bac: Button
+    lateinit var bc: Button
+    lateinit var bbrac1: Button
+    lateinit var bbrac2: Button
+    lateinit var bsin: Button
+    lateinit var bcos: Button
+    lateinit var btan: Button
+    lateinit var blog: Button
+    lateinit var bln: Button
+    lateinit var bfact: Button
+    lateinit var bsquare: Button
+    lateinit var bsqrt: Button
+    lateinit var binv: Button
+    lateinit var b0: Button
+    lateinit var b9: Button
+    lateinit var b8: Button
+    lateinit var b7: Button
+    lateinit var b6: Button
+    lateinit var b5: Button
+    lateinit var b4: Button
+    lateinit var b3: Button
+    lateinit var b2: Button
+    lateinit var b1: Button
+    lateinit var bpi: Button
+    lateinit var bmul: Button
+    lateinit var bminus: Button
+    lateinit var bplus: Button
+    lateinit var bequal: Button
+    lateinit var bdot: Button
+    lateinit var bdiv: Button
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
+
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
--- app/src/main/java/org/techtales/bottomnavapp/MainActivity.kt
+++ app/src/main/java/org/techtales/bottomnavapp/MainActivity.kt
@@ -2,22 +2,55 @@ package org.techtales.bottomnavapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import org.techtales.bottomnavapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
+    //Assign fragment to Variable
+    val homeFragment = HomeFragment()
+    val personFragment = PersonFragment()
+    val settingsFragment = SettingsFragment()
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        //Assign fragment to Variable
-        val homeFragment = HomeFragment()
-        val personFragment = PersonFragment()
-        val settingsFragment = SettingsFragment()
+        actionBarDrawerToggle = ActionBarDrawerToggle(this,binding.drawerlayout, R.string.nav_open, R.string.nav_close)
+
+        binding.drawerlayout.addDrawerListener(actionBarDrawerToggle)
+        actionBarDrawerToggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+
 
         setCurrentFragment(homeFragment)
+
+        binding.navview.setNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.home ->{
+                    setCurrentFragment(homeFragment)
+                    //close drawer when we click new item
+                    binding.drawerlayout.closeDrawers()
+                }
+                R.id.person ->{
+                    setCurrentFragment(personFragment)
+                    binding.drawerlayout.closeDrawers()
+                }
+                R.id.setting ->{
+                    setCurrentFragment(settingsFragment)
+                    binding.drawerlayout.closeDrawers()
+                }
+            }
+            true
+        }
+
         //set navigation item selected listener for get the navigation translation
         binding.bottomBar.setOnNavigationItemSelectedListener {
 
@@ -29,6 +62,14 @@ class MainActivity : AppCompatActivity() {
             }
             true
         }
+    }
+
+    // drawer open close
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return if(actionBarDrawerToggle.onOptionsItemSelected(item)){
+            true
+        }
+        else super.onOptionsItemSelected(item)
     }
 
     //create a method for set fragment and simplify the code for reassign
